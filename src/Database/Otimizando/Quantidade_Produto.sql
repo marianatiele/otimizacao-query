1964e34a11cb581554df063afe89d9eb814b6485
Maria Natiele <106673906+marianatiele@users.noreply.github.com>
2025-10-23 20:04:57 +0000

    add consulta otimizada

--- src/Database/Otimizando/Quantidade_Produto.sql
+++ src/Database/Otimizando/Quantidade_Produto.sql
@@ -1,3 +1,5 @@
+-- Consulta  sem index 0,04779  com index 0,02943
+
 SELECT 
     c.Cod_Categoria,
     c.Tipo,
@@ -13,3 +15,15 @@ HAVING
     COUNT(p.Cod_Produto) >= 3
 ORDER BY 
     Quantidade_Produtos DESC;
+
+
+
+
+create nonclustered index ix_produto_2 
+on produto (cod_categoria) include (nome_produto, valor_unitario, cod_produto);
+
+
+-- drop index ix_produto_2 on produto;
+
+
+
